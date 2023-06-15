8c42b95ba1697dc3c53eb1d2b4ab6c5fd71b3c9f
Khushnidjon <khushnidgit@gmail.com>
2023-06-15 17:53:53 +0500

    Displays mapped resource on successful loading

--- EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Shared Presentation/LoadResourcePresenterTests.swift
@@ -9,85 +9,90 @@ import XCTest
 import EssentialFeed
 
 class LoadResourcePresenterTests: XCTestCase {
-
-   func test_init_doesNotSendMessagesToView() {
-       let (_, view) = makeSUT()
-       
-       XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
-   }
-   
-   func test_didStartLoading_displaysNoErrorMessageAndStartsLoading() {
-       let (sut, view) = makeSUT()
-       
-       sut.didStartLoading()
-       
-       XCTAssertEqual(view.messages, [
-           .display(errorMessage: nil),
-           .display(isLoading: true)
-       ])
-   }
-   
-   func test_didFinishLoadingFeed_displaysFeedAndStopsLoading() {
-       let (sut, view) = makeSUT()
-       let feed = uniqueImageFeed().models
-       
-       sut.didFinishLoadingFeed(with: feed)
-       
-       XCTAssertEqual(view.messages, [
-           .display(feed: feed),
-           .display(isLoading: false)
-       ])
-   }
-   
-   func test_didFinishLoadingFeed_displaysErrorAndStopLoxading() {
-       let (sut, view) = makeSUT()
-       
-       sut.didFinishLoadingFeed(with: anyNSError())
-       
-       XCTAssertEqual(view.messages, [
-           .display(errorMessage: localized("FEED_VIEW_CONNECTION_ERROR")),
-           .display(isLoading: false)
-       ])
-   }
-   
-   // MARK: - Helpers
-   
-   private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: LoadResourcePresenter, view: ViewSpy) {
-       let view = ViewSpy()
-       let sut = LoadResourcePresenter(feedView: view, loadingView: view, errorView: view)
-       trackForMemoryLeaks(for: view, sut, file: file, line: line)
-       return (sut, view)
-   }
-   
-   private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
-       let table = "Feed"
-       let bundle = Bundle(for: LoadResourcePresenter .self)
-       let value = bundle.localizedString(forKey: key, value: nil, table: table)
-       if value == key {
-           XCTFail("Missing localized string for key: \(key) in table \(table)", file: file, line: line)
-       }
-       return value
-   }
-
-   private class ViewSpy: FeedView, FeedErrorView, FeedLoadingView {
-       private(set) var messages = Set<Message>()
-
-       func display(_ viewModel: FeedViewModel) {
-           messages.insert(.display(feed: viewModel.feed))
-       }
-       
-       func display(_ viewModel: FeedErrorViewModel) {
-           messages.insert(.display(errorMessage: viewModel.message))
-       }
-       
-       func display(_ viewModel: FeedLoadingViewModel) {
-           messages.insert (.display(isLoading: viewModel.isLoading))
-       }
-       
-       enum Message: Hashable {
-           case display(feed: [FeedImage])
-           case display(errorMessage: String?)
-           case display(isLoading: Bool)
-       }
-   }
+    
+    func test_init_doesNotSendMessagesToView() {
+        let (_, view) = makeSUT()
+        
+        XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
+    }
+    
+    func test_didStartLoading_displaysNoErrorMessageAndStartsLoading() {
+        let (sut, view) = makeSUT()
+        
+        sut.didStartLoading()
+        
+        XCTAssertEqual(view.messages, [
+            .display(errorMessage: nil),
+            .display(isLoading: true)
+        ])
+    }
+    
+    func test_didFinishLoadingResource_displaysResourceAndStopsLoading() {
+        let (sut, view) = makeSUT(mapper: { resource in
+            resource + " view model"
+        })
+        
+        sut.didFinishLoading(with: "resource")
+        
+        XCTAssertEqual(view.messages, [
+            .display(resourceViewModel: "resource view model"),
+            .display(isLoading: false)
+        ])
+    }
+    
+    func test_didFinishLoadingFeed_displaysErrorAndStopLoxading() {
+        let (sut, view) = makeSUT()
+        
+        sut.didFinishLoadingFeed(with: anyNSError())
+        
+        XCTAssertEqual(view.messages, [
+            .display(errorMessage: localized("FEED_VIEW_CONNECTION_ERROR")),
+            .display(isLoading: false)
+        ])
+    }
+    
+    // MARK: - Helpers
+    
+    private func makeSUT(
+        mapper: @escaping LoadResourcePresenter.Mapper = { _ in "any" },
+        file: StaticString = #file,
+        line: UInt = #line
+    ) -> (sut: LoadResourcePresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = LoadResourcePresenter(resourceView: view, loadingView: view, errorView: view, mapper: mapper)
+        trackForMemoryLeaks(for: view, sut, file: file, line: line)
+        return (sut, view)
+    }
+    
+    private func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: LoadResourcePresenter .self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table \(table)", file: file, line: line)
+        }
+        return value
+    }
+    
+    private class ViewSpy: ResourceView, FeedErrorView, FeedLoadingView {
+        private(set) var messages = Set<Message>()
+        
+        func display(_ viewModel: String) {
+            messages.insert(.display(resourceViewModel: viewModel))
+        }
+        
+        func display(_ viewModel: FeedErrorViewModel) {
+            messages.insert(.display(errorMessage: viewModel.message))
+        }
+        
+        func display(_ viewModel: FeedLoadingViewModel) {
+            messages.insert (.display(isLoading: viewModel.isLoading))
+        }
+        
+        enum Message: Hashable {
+            case display(resourceViewModel: String)
+            case display(errorMessage: String?)
+            case display(isLoading: Bool)
+        }
+    }
 }
