0533a1558bf6e1698a867f3973ea314f57c25d91
Khushnidjon <khushnidgit@gmail.com>
2023-09-13 16:13:37 +0500

    Update Feed API URL

--- EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialAppTests/FeedAcceptanceTests.swift
@@ -11,15 +11,15 @@ import EssentialFeediOS
 @testable import EssentialApp
 
 class FeedAcceptanceTests: XCTestCase {
-
+    
     func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let feed = launch(httpClient: .online(response), store: .empty)
-
+        
         XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 2)
         XCTAssertEqual(feed.renderedFeedImageData(at: 0), makeImageData())
         XCTAssertEqual(feed.renderedFeedImageData(at: 1), makeImageData())
     }
-
+    
     func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHaNoConnectivity() {
         let sharedStore = InMemoryFeedStore.empty
         let onlineFeed = launch(httpClient: .online(response), store: sharedStore)
@@ -32,7 +32,7 @@ class FeedAcceptanceTests: XCTestCase {
         XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 0), makeImageData())
         XCTAssertEqual(offlineFeed.renderedFeedImageData(at: 1), makeImageData())
     }
-
+    
     func test_onLaunch_displaysEmptyFeedWhenCustomerHasNoConnectivityAndNoCache() {
         let feed = launch()
         
@@ -54,14 +54,14 @@ class FeedAcceptanceTests: XCTestCase {
         
         XCTAssertNotNil(store.feedCache, "Expected to keep non-expired cache")
     }
-
+    
     // MARK: - Helpers
-
+    
     private func launch(httpClient: HTTPClientStub = .offline, store: InMemoryFeedStore = .empty) -> ListViewController {
         let sut = SceneDelegate(httpClient: httpClient, store: store)
         sut.window = UIWindow()
         sut.configureWindow()
-
+        
         let nav = sut.window?.rootViewController as? UINavigationController
         return nav?.topViewController as! ListViewController
     }
@@ -75,26 +75,29 @@ class FeedAcceptanceTests: XCTestCase {
         let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
         return (makeData(for: url), response)
     }
-
+    
     private func makeData(for url: URL) -> Data {
-        switch url.absoluteString {
-        case "http://image.com":
+        switch url.path {
+        case "/image-1", "/image-2":
             return makeImageData()
-
-        default:
+            
+        case "/essential-feed/v1/feed":
             return makeFeedData()
+            
+        default:
+            return Data()
         }
     }
-
+    
     private func makeImageData() -> Data {
         return UIImage.make(withColor: .red).pngData()!
     }
-
+    
     private func makeFeedData() -> Data {
         return try! JSONSerialization.data(withJSONObject: ["items": [
-            ["id": UUID().uuidString, "image": "http://image.com"],
-            ["id": UUID().uuidString, "image": "http://image.com"]
+            ["id": "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086", "image": "http://feed.com/image-1"],
+            ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-2"]
         ]])
     }
-
+    
 }
