a9adb46fca7718db766ebfb3cd590f98294b5a3e
Khushnidjon <khushnidgit@gmail.com>
2023-10-10 17:19:55 +0500

    Prevent load more action while loading more

--- EssentialApp/FeedViewAdapter.swift
+++ EssentialApp/FeedViewAdapter.swift
@@ -15,6 +15,7 @@ final class FeedViewAdapter: ResourceView {
     private let selection: (FeedImage) -> Void
     
     private typealias ImageDataPresentationAdapter = LoadResourcePresentationAdapter<Data, WeakReferenceVirtialProxy<FeedImageCellController>>
+    private typealias LoadMorePresentationAdapter = LoadResourcePresentationAdapter<Paginated<FeedImage>, FeedViewAdapter>
 
     init(controller: ListViewController, imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher, selection: @escaping (FeedImage) -> Void) {
         self.controller = controller
@@ -44,11 +45,21 @@ final class FeedViewAdapter: ResourceView {
             return CellController(id: model, view)
         }
         
-        let loadMore = LoadMoreCellController {
-            viewModel.loadMore?({ _ in })
+        guard let loadMorePublisher = viewModel.loadMorePublisher else {
+            controller?.display(feed)
+            return
         }
-        let loadMoreSection = [CellController(id: UUID(), loadMore)]
         
+        let loadMoreAdapter = LoadMorePresentationAdapter(loader: loadMorePublisher)
+        let loadMore = LoadMoreCellController(callback: loadMoreAdapter.loadResource)
+        
+        loadMoreAdapter.presenter = LoadResourcePresenter(
+            resourceView: self,
+            loadingView: WeakReferenceVirtialProxy(loadMore),
+            errorView: WeakReferenceVirtialProxy(loadMore)
+        )
+        
+        let loadMoreSection = [CellController(id: UUID(), loadMore)]
         controller?.display(feed, loadMoreSection)
     }
 }
